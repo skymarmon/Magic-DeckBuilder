e00b29d42819c792cb5c9bf037d86ccf95d9fdcc
skymarmon <pshsky0612@naver.com>
2025-05-09 20:31:43 +0900

    mmmm

--- scenes/Images.js
+++ scenes/Images.js
@@ -6,9 +6,9 @@ export default class Images extends Phaser.Scene {
     preload() {
         this.load.image('credit', 'assets/images/credit.png');
         this.load.image('main', 'assets/images/main.png');
-        this.load.image('research', 'assets/images/button_research.png');
-        this.load.image('start', 'assets/images/button_start.png');
-        this.load.image('record', 'assets/images/button_record.png');
+        this.load.image('button_research', 'assets/images/button_research.png');
+        this.load.image('button_start', 'assets/images/button_start.png');
+        this.load.image('button_record', 'assets/images/button_record.png');
     }
 
     create() {
