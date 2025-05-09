e792fa60825db0871ee966e8a44cd85243aed51a
skymarmon <pshsky0612@naver.com>
2025-05-09 20:57:06 +0900

    mm

--- scenes/Lobby.js
+++ scenes/Lobby.js
@@ -3,41 +3,45 @@ export default class Lobby extends Phaser.Scene {
         super('Lobby');
     }
 
+    init(data) {
+        this.fromLicense = data.fromLicense || false;
+    }
+
     create() {
         const { width, height } = this.sys.game.canvas;
 
-        // 배경 이미지 설정
-        const bg = this.add.image(width / 2, height / 2, 'main').setAlpha(0);
+        const bg = this.add.image(width / 2, height / 2, 'main');
         const iw = this.textures.get('main').getSourceImage().width;
         const ih = this.textures.get('main').getSourceImage().height;
         const scale = Math.min(width / iw, height / ih);
         bg.setScale(scale);
 
-        this.tweens.add({
-            targets: bg,
-            alpha: 1,
-            duration: 1000,
-            ease: 'Linear',
-        });
-
         const displayWidth = iw * scale;
         const displayHeight = ih * scale;
 
-        // 기준 위치 (배경 기준 우측 하단)
         const baseX = width / 2 + displayWidth / 2;
         const baseY = height / 2 + displayHeight / 2;
 
-        // 버튼 위치 조정
-        const button_start = this.add.image(baseX - 180, baseY - 230, 'button_start').setInteractive();
-        button_start.setScale(0.7);
+        // 버튼 미리 생성
+        const button_start = this.add.image(baseX - 600, baseY - 230, 'button_start').setScale(0.7).setAlpha(this.fromLicense ? 0 : 1).setInteractive();
         button_start.on('pointerdown', () => this.scene.start('Class'));
 
-        const button_research = this.add.image(baseX - 300, baseY - 120, 'button_research').setInteractive();
-        button_research.setScale(0.5);
+        const button_research = this.add.image(baseX - 300, baseY - 120, 'button_research').setScale(0.5).setAlpha(this.fromLicense ? 0 : 1).setInteractive();
         button_research.on('pointerdown', () => this.scene.start('Research'));
 
-        const button_record = this.add.image(baseX - 140, baseY - 120, 'button_record').setInteractive();
-        button_record.setScale(0.5);
+        const button_record = this.add.image(baseX - 140, baseY - 120, 'button_record').setScale(0.5).setAlpha(this.fromLicense ? 0 : 1).setInteractive();
         button_record.on('pointerdown', () => this.scene.start('Record'));
+
+        // 배경 투명도
+        if (this.fromLicense) {
+            bg.setAlpha(0);
+
+            this.tweens.add({
+                targets: [bg, button_start, button_research, button_record],
+                alpha: 1,
+                duration: 1000,
+                ease: 'Linear'
+            });
+        }
     }
 }
