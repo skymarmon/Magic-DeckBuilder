075b95d6c6c0fcaeffe7772921c530d56197656f
skymarmon <pshsky0612@naver.com>
2025-05-09 13:01:38 +0900

    m

--- scenes/License.js
+++ scenes/License.js
@@ -4,8 +4,8 @@ export default class License extends Phaser.Scene {
     }
 
     create() {
-        const image = this.add.image(window.innerWidth / 2, window.innerHeight / 2 + 150, 'credit').setAlpha(0);
-        image.setScale(0.35);
+        const image = this.add.image(window.innerWidth / 2, window.innerHeight / 2 + 200, 'credit').setAlpha(0);
+        image.setScale(0.3);
 
         this.tweens.add({
             targets: image,
