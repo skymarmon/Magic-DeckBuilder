defa2fa5c2afb97bb7e18b9b73251d320edb104c
skymarmon <pshsky0612@naver.com>
2025-05-10 13:52:40 +0900

    a

--- scenes/Images.js
+++ scenes/Images.js
@@ -12,6 +12,7 @@ export default class Images extends Phaser.Scene {
         this.load.image('text_choose_class', 'assets/images/text_choose_class.png');
         this.load.image('class_backspace', 'assets/images/class_backspace.png');
         this.load.image('button_location', 'assets/images/button_location.png');
+        this.load.image('class_wizard', 'assets/images/class_wizard.png');
     }
 
     create() {
