405222dec158b008a453996fb86d3e016742decf
skymarmon <pshsky0612@naver.com>
2025-05-11 09:29:26 +0900

    a

--- scenes/Field.js
+++ scenes/Field.js
@@ -6,10 +6,10 @@ export default class Field extends Phaser.Scene {
     create() {
         const { width, height } = this.sys.game.canvas;
 
-        // 배경 색상 설정
+        // 배경
         this.cameras.main.setBackgroundColor('#dddddd');
 
-        // 무한 맵 개념: 실제로는 넓은 범위 설정
+        // 무한 맵 설정
         const worldSize = 10000;
         this.physics.world.setBounds(-worldSize / 2, -worldSize / 2, worldSize, worldSize);
 
@@ -20,8 +20,13 @@ export default class Field extends Phaser.Scene {
         this.player.setDrag(0.95);
         this.player.setMaxVelocity(200);
 
-        // 카메라 설정
-        this.cameras.main.startFollow(this.player, true, 0.15, 0.15); // 부드러운 따라가기
+        // 캐릭터 크기 화면 기준으로 20% 축소
+        const originalWidth = this.textures.get('field_character').getSourceImage().width;
+        const scale = Math.min((width * 0.1) / originalWidth, 1); // 대략 화면 폭의 10% → 원래 크기의 20% 수준
+        this.player.setScale(scale);
+
+        // 카메라
+        this.cameras.main.startFollow(this.player, true, 0.05, 0.05);
         this.cameras.main.setBounds(-worldSize / 2, -worldSize / 2, worldSize, worldSize);
 
         // 키 입력
@@ -39,17 +44,11 @@ export default class Field extends Phaser.Scene {
         let vx = 0;
         let vy = 0;
 
-        if (this.cursors.left.isDown) {
-            vx = -speed;
-        } else if (this.cursors.right.isDown) {
-            vx = speed;
-        }
+        if (this.cursors.left.isDown) vx = -speed;
+        else if (this.cursors.right.isDown) vx = speed;
 
-        if (this.cursors.up.isDown) {
-            vy = -speed;
-        } else if (this.cursors.down.isDown) {
-            vy = speed;
-        }
+        if (this.cursors.up.isDown) vy = -speed;
+        else if (this.cursors.down.isDown) vy = speed;
 
         this.player.setVelocity(vx, vy);
     }
